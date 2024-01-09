ffaa4b100918b0d3dd9d45ba302bc4a7f871c0cf
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-01-09 11:40:41 -0300

    signin justify center

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+// import Home from "./pages/Home"
+import Signin from "./pages/Signin";
 
 function App() {
 
-  return <h1 className="text-3xl font-bold underline"> Hello!</h1>
+  return (
+
+    <main className="flex items-center justify-center h-screen">
+
+      <Signin/>
+
+    </main>
+
+  );
+
 
 }
 
