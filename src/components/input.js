446cbb91355d7fa7013decba948cb5875e200ed5
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-01-09 09:51:39 -0300

    thi-wallet

---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+export default function Input(){
+
+    return(
+
+        <Input
+            type="password"
+            placeholder="Password"
+            className="rounded p-2 w-full"
+        />
+
+    );
+
+}
